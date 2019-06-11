4612069e9df6a793b2dd469fdd1d55ec80512cfd
dpnhat.nguyen@gmail.com <dpnhat.nguyen@gmail.com>
2019-06-11 20:00:12 +0700

    Host & MinSDK updated
    More steps for Host
    MinSDK is now 26

--- app/src/main/java/com/orbital19/imabip/Host.java
+++ app/src/main/java/com/orbital19/imabip/Host.java
@@ -24,11 +24,10 @@ import java.util.ArrayList;
 
 
 public class Host extends AppCompatActivity {
-    private EditText inName, inType, inVenue, inTime, inDesc, inFilled, inPax;
+    private EditText inName, inType, inVenue, inMonth, inDate, inAMorPM, inDesc, inFilled, inPax;
     private Button hostBtn;
-    private String[] inputs = new String[7];
+    private String[] inputs = new String[9];
 
-    @SuppressWarnings("unused")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,7 +42,9 @@ public class Host extends AppCompatActivity {
         inName = findViewById(R.id.input_name);
         inType = findViewById(R.id.input_type);
         inVenue = findViewById(R.id.input_venue);
-        inTime = findViewById(R.id.input_time);
+        inMonth = findViewById(R.id.input_month);
+        inDate = findViewById(R.id.input_date);
+        inAMorPM = findViewById(R.id.input_AM_or_PM);
         inDesc = findViewById(R.id.input_description);
         inFilled = findViewById(R.id.input_filled);
         inPax = findViewById(R.id.input_pax);
@@ -57,19 +58,34 @@ public class Host extends AppCompatActivity {
                 inputs[0] = inName.getText().toString();
                 inputs[1] = inType.getText().toString();
                 inputs[2] = inVenue.getText().toString();
-                inputs[3] = inTime.getText().toString();
-                inputs[4] = inDesc.getText().toString();
-                inputs[5] = inFilled.getText().toString();
-                inputs[6] = inPax.getText().toString();
+                inputs[3] = inMonth.getText().toString();
+                inputs[4] = inDate.getText().toString();
+                inputs[5] = inAMorPM.getText().toString();
+                inputs[6] = inDesc.getText().toString();
+                inputs[7] = inFilled.getText().toString();
+                inputs[8] = inPax.getText().toString();
 
+                boolean valid = false;
 
+                while (!valid) {
+                    try {
+                        addNewEvent();
+                        valid = true;
+                    } catch (Exception e) {
+                        faultyToast();
+                        valid = false;
+                    }
+                }
 
-                addNewEvent();
                 addedToast();
                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
                 finish();
             }
         });
+    }
+
+    private void faultyToast() {
+        Toast.makeText(getApplicationContext(), "Invalid Data", Toast.LENGTH_LONG).show();
     }
 
     private boolean isLegitLetter(char c) {
@@ -105,8 +121,8 @@ public class Host extends AppCompatActivity {
                 contact.add((String) doc.get(User.phoneKey));
 
                 Event ev = new Event(contact, inputs[4], (String) doc.get(User.idKey), inputs[0],
-                        inputs[1], inputs[2], inputs[3],
-                        Long.parseLong(inputs[6]), Long.parseLong(inputs[5]));
+                        inputs[1], inputs[2], inputs[3] + "at" + inputs[4] + inputs[5],
+                        Long.parseLong(inputs[8]), Long.parseLong(inputs[7]));
 
                 ev.createEntry();
 
