7fe8759f4230d905647bd07117fa5828ffd851e5
dpnhat.nguyen@gmail.com <dpnhat.nguyen@gmail.com>
2019-07-22 22:19:28 +0700

    Initiated fragment for teams browsing
    Blank fragment

---
+++ app/src/main/java/com/orbital19/imabip/teams/fragment_browse_teams.java
@@ -0,0 +1,39 @@
+package com.orbital19.imabip.teams;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.orbital19.imabip.R;
+
+
+public class fragment_browse_teams extends Fragment {
+
+    public fragment_browse_teams() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_browse_teams, container, false);
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
